d22765bb0090f9578de2807ace9ed3a8fb30ce96
user0032 <70772813+PCuser0032@users.noreply.github.com>
2022-03-21 16:32:29 +0300

    Add info about author

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using MyFormatOutput;
 
 Console.WriteLine("Complex number arithmetic implementation on C#.");
+Console.WriteLine("Created by Kozlov D., group VPI-31.");
 
 MyComplex a = new MyComplex() { Re = 3.0, Im = 7.0 };
 MyComplex b = new MyComplex() { Re = 2.0, Im = 3.0 };
